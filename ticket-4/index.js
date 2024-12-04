2291e046653f84c7b2b9be3615cca49f2a0015d0
sumeyacodes <sumeya4h@gmail.com>
2024-12-04 17:01:19 +0000

    completed tickets 4 & 5

--- ticket-4/index.js
+++ ticket-4/index.js
@@ -1,15 +1,20 @@
 function getGradeFromPoints(points) {
-  if ((points) => 90) {
+  if ((points) >= 90) {
     return "A";
-  } else if (Points >= 70) {
+  } else if (points >= 70) {
     return "B";
-  } else if (pointss >= 50) {
+  } else if (points >= 50) {
     return "C";
-  } else if (poInts >= 40) {
+  } else if (points >= 40) {
     return "D";
   }
   return "E";
 }
 
 const grade = getGradeFromPoints(90);
-console.log(Grade, "should be A");
+console.log(grade + " should be A");
+
+// 1. Check the syntax/structure of code (no typos)
+// 2. Test the function and check output in devtools
+// 3. Follow the code per line and articulate what is happening explicitly to understand how the function works
+//     - eg. check data types, return statements, if statement logic
--- ticket-5/index.js
+++ ticket-5/index.js
@@ -6,7 +6,6 @@ const shoppingCart = [
   },
   {
     product: "Apples",
-
     price: 0.7,
     quantity: 8,
   },
@@ -19,9 +18,10 @@ const shoppingCart = [
 
 let totalCost = 0;
 
-for (let i = 0; i <= shoppingCart.length; i++) {
+for (let i = 0; i < shoppingCart.length; i++) { //index should run while less than shopping cart array length
   const item = shoppingCart[i];
-  const costForItem = item.price * item.quantity;
+  console.log(item)
+  const costForItem = item.price * item.quantity; // problem
   totalCost += costForItem;
 }
 
@@ -30,4 +30,32 @@ const formatter = new Intl.NumberFormat("en-GB", {
   currency: "GBP",
 });
 
+
 console.log(`The total cost for this cart is ${formatter.format(totalCost)}`);
+
+
+// 1. Check the syntax/structure of code (no typos)
+// 2. Test the function and check output in devtools
+// 3. Follow the code per line and articulate what is happening explicitly to understand how the function works
+//     - eg. check data types, return statements, if statement logic
+// 4. If we are making changes to the logic of the function, then test and log each assumption + changes
+
+// ERROR MESSAGE IN CONSOLE: Uncaught TypeError: Cannot read properties of undefined (reading 'price') - line 23
+
+// 1. FAIL: It can't access item.price because of the nesting of the array (structure) or the datatype. 
+    // Test: console log the type of data for item to get more info
+    // Outcome: we've been able to access item.price within the for loop, and datatypes seme to match (property quant & price are both numbers)
+    // Test 2: console logged cost for item, returned the costs for each object of array correctly (as we expected)
+    // outcome: datatypes are consistent (numbers for item.price property & .quantity)
+    
+// 2. FAIL: Issue might be how the total cost and cost for item variables are added?
+    // Test: i can't remember but it worked
+
+// 3. FAIL: what if we store item.price and item.quantity in seperate variables before multiplying them ? 
+    // Reason: As the issue is that it can't read the price property from item
+    // Test: seperate item.price and item.quantity in seperate variables before multiplying
+    // outcome: didn't make a diff -- FAIL
+
+// 4. SUCCESS: for loop logic? <= or just < ?
+    // test: change forloop logic to i < shoppingCart.length so it iterates over the array less than the shopping items array length
+    // outcome: worked
